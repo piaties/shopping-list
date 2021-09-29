1d8f12cc21203296090015af295ead8229a04cd6
Mahad Ahmed <mahmed3@cnm.edu>
2021-09-29 14:15:53 -0700

    test`` issue

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function setDeleteButtonEvent(id){
     });
 }
 function createListItemHtml(itemName, itemAmount, id) {
-    return `<li id="item${id}">  ${itemName} - ${itemAmount} +
+    return `<li id="item${id}">  ${itemName} - ${itemAmount}
         <button id="button${id}" type="button">Delete Item</button> </li>`;
 }
 
